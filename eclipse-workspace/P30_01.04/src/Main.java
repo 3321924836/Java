aea8ba3e403dfcd9f4c2ca702499eb031e683e68
Leslie <LeslieLiuZJNU@users.noreply.github.com>
2019-10-19 09:27:19 +0800

    none

---
+++ eclipse-workspace/P30_01.04/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("a     a^2   a^3\n1     1     1\n2     4     8\n3     9     27\n4     16    64");
+    }
+}
